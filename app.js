78bc068643a66cff8f5577db8a42ca6b7c27b16e
Brent Meyer <brent.e.meyer@gmail.com>
2015-12-04 17:35:27 -0500

    Adds view, data to view, and some styles

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var express = require("express");
 
 var app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
 
   var today = moment().format("MM/DD/YYYY");
@@ -80,7 +83,7 @@ app.get('/', function(req, res) {
 
     });
 
-    res.send(matchups);
+    res.render('index', { matchups: matchups });
 
   });
 
@@ -90,5 +93,5 @@ var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('listening @ http://' + host + ':' + port);
+  console.log('listening @ http://localhost:' + port);
 });
