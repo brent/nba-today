74a6d6b08162622bc80db19580f3b970570324cc
Brent Meyer <brent.e.meyer@gmail.com>
2015-12-21 01:48:53 -0500

    Adds first pass at displaying scores

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var nba = require("nba");
 var moment = require("moment");
 var express = require("express");
+var fs = require('fs');
 
 var app = express();
 
@@ -9,19 +10,26 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res) {
 
-  var today = moment().format("MM/DD/YYYY");
+  var today = moment().subtract(1, 'days').format("MM/DD/YYYY");
+  // var today = moment().format("MM/DD/YYYY");
 
   nba.stats.scoreboard({gameDate: today}, function(err, response) {
 
+    console.log(response);
+
     var matchups = [];
+
     // combine east/west rosters so it can be looped through later
     var allTeams = response.eastConfStandingsByDay.concat(response.westConfStandingsByDay);
+    var lineScore = response.lineScore;
 
     response.gameHeader.forEach(function(el, index, arr) {
 
       // relevant stats
-      var lastMeeting = response.lastMeeting[index]
-        , homeTeamId = el.homeTeamId
+      var lastMeeting = response.lastMeeting[index],
+          matchupId = el.gameId;
+
+      var homeTeamId = el.homeTeamId
         , homeTeamCity = lastMeeting.lastGameHomeTeamCity
         , homeTeamName = lastMeeting.lastGameHomeTeamName
         , homeTeamAbbreviation = lastMeeting.lastGameHomeTeamAbbreviation
@@ -29,6 +37,7 @@ app.get('/', function(req, res) {
         , homeTeamLosses = 0
         , homeTeamHomeRecord = ""
         , homeTeamRoadRecord = ""
+        , homeTeamScore = 0
         , visitorTeamId = el.visitorTeamId
         , visitorTeamCity = lastMeeting.lastGameVisitorTeamCity
         , visitorTeamName = lastMeeting.lastGameVisitorTeamName
@@ -36,7 +45,9 @@ app.get('/', function(req, res) {
         , visitorTeamWins = 0
         , visitorTeamLosses = 0
         , visitorTeamHomeRecord = ""
-        , visitorTeamRoadRecord = "";
+        , visitorTeamRoadRecord = ""
+        , visitorTeamScore = 0
+      ;
 
       // loop through roster to find home/away team stats
       allTeams.forEach(function(el, index, arr) {
@@ -54,6 +65,15 @@ app.get('/', function(req, res) {
           }
       });
 
+      lineScore.forEach(function(el, index, arr) {
+        if(matchupId == el.gameId && el.teamId == homeTeamId) {
+          homeTeamScore = el.pts;
+        }
+        if(matchupId == el.gameId && el.teamId == visitorTeamId) {
+          visitorTeamScore = el.pts;
+        }
+      });
+
       // add stats to matchups array
       matchups.push({
         date: response.gameHeader[index].gameDateEst,
@@ -67,7 +87,8 @@ app.get('/', function(req, res) {
           wins: homeTeamWins,
           losses: homeTeamLosses,
           homeRecord: homeTeamHomeRecord,
-          roadRecord: homeTeamRoadRecord
+          roadRecord: homeTeamRoadRecord,
+          score: homeTeamScore
         },
         visitorTeam: {
           id: visitorTeamId,
@@ -77,7 +98,8 @@ app.get('/', function(req, res) {
           wins: visitorTeamWins,
           losses: visitorTeamLosses,
           homeRecord: visitorTeamHomeRecord,
-          roadRecord: visitorTeamRoadRecord
+          roadRecord: visitorTeamRoadRecord,
+          score: visitorTeamScore
         }
       });
 
